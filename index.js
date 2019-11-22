6663f176277386463340d70d0f5885ca464e087d
Monica  Karasek <mlkarasek@gmail.com>
2019-11-22 20:51:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,18 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+  //...
+]
+
+function superbowlWin(record) {
+  let result = record.find => result === "W";
+  return !!result ? reult.year : undefined 
+}
+
+
+
