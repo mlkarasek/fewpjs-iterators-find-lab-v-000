87c827560d1c38cb199029ffc578c3aa0ea40809
Monica  Karasek <mlkarasek@gmail.com>
2019-11-22 20:53:22 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+  //...
+]
+
+function superbowlWin(record) {
+  let result = record.find(record => record.result === "W");
+  return !!result ? result.year : undefined
+}
